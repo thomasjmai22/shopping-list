4ff6cf31fc0f0706860139bb7b441e2663d4ccad
thomasjmai22 <thomas.j.mai22@gmail.com>
2020-06-09 16:54:57 -0700

    updated js

--- index.js
+++ index.js
@@ -32,38 +32,3 @@ $(function updateList() {
 });
 
 $(updateList);
-
-//input item
-//enter "Return"
-//or CLICK button
-
-/*
-DOM ready
-jQuery runs
-
-jQuery runs -> throws error -> cancels all JS on the page
-DOM ready
-
-*/
-
-//CLICK button to check or uncheck
-
-//"delete" button CLICK to remove item from list
-
-// INSTRUCTIONS
-// enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
-
-// check and uncheck items on the list by clicking the "Check" button
-
-// permanently remove items from the list
-
-//USE THE FOLLOWING JQuery methods
-// .submit()          |||The submit event occurs when a form is submitted. -only used on <form>|||
-
-// preventDefault()  |||The currentTarget event property returns the element whose event listeners triggered the event.|||
-
-// toggleClass()     |||toggles between adding and removing one or more class names from the selected elements|||
-
-// closest()          |||returns the first ancestor of the selected element. -runs up the DOM tree: span > li > ul > div > body|||
-
-//*use THIS and event delegation//
